5ce5a8285e7c11e057f12b8c26897c29bde50ed3
Ooi Zhen Zhi <tp060844@mail.apu.edu.my>
2023-04-15 01:01:24 +0800

    User authentication
    login working

---
+++ dummy.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>KON Quiz</title>
+        <meta name="description" content="Our first page">
+        <meta name="keywords" content="html tutorial template">
+    </head>
+    
+    <body>
+        <?php session_start();?>
+        <h3>Login homepage test.</h3>
+        <?php echo "This is the data of the user: ". $_SESSION['username'];?>
+    </body>
+</html>
--- login.php
+++ login.php
@@ -6,19 +6,48 @@
     </head>
     
     <body>
-        <form action="" method="POST">
+        <?php
+            include("conn.php");
+            session_start();
+            if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+                $credential = mysqli_real_escape_string($con,$_POST['credential']);
+                $password = mysqli_real_escape_string($con,$_POST['password']);
+                
+                $query = "SELECT id, username, email, password FROM user where username = '$credential' OR email = '$credential'";
+                $result = mysqli_query($con, $query);
+                $data = $result->fetch_assoc();
+
+                if ($data == NULL) {
+                    echo "<script>alert('Username or password incorrect!')</script>";
+                    //return;
+                }
+                else if ($password == $data['password']) {
+                    $_SESSION['email'] = $data['email'];
+                    $_SESSION['id'] = $data['id'];
+                    $_SESSION['username'] = $data['username'];
+                    header("location:dummy.php");
+                }
+                else {
+                    echo "<script>alert('Username or password incorrect!')</script>";
+                }
+                mysqli_close($con);
+            }
+        ?>
+
+        <!-- html start here -->
+        <form action="login.php" method="POST">
             <table>
                 <tr>    
                     <td>Username or Email:</td>
                 </tr>
                 <tr>
-                    <td><input type="text" name="username"></td>
+                    <td><input type="text" name="credential"></td>
                 </tr>
                 <tr>
                     <td>Password:</td>
                 </tr>    
                 <tr>
-                    <td><input type="text" name="password_1"></td>
+                    <td><input type="password" name="password"></td>
                 </tr>
                 <tr>
                     <td><button name="login">Login</button></td>
