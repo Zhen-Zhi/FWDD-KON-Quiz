80fc73a09a55244bb309393bff4f95a5944dec89
Tunardo <justinkwy6246@gmail.com>
2023-04-23 04:42:24 +0800

    footer

---
+++ footer.php
@@ -0,0 +1,51 @@
+<?php include("template.html"); ?>
+
+<br>
+<br>
+
+<footer class="container-fluid footer px-5">
+    <div class="row">
+        <div class="col-md-4">
+            <h5>KON Quiz</h5>
+            <h6>Challenge your mind, enrich your knowledge.</h6>
+        </div>
+        <div class="col-md-4 d-flex flex-column">
+            <h5>Support</h5>
+            <h7><a href="">About Us</a></h7>
+            <h7><a href="">Feedback</a></h7>
+            <h7><a href="">Frequent Asked Question (FAQ)</a></h7>
+        </div>
+        <div class="col-md-4">
+            <h5>Social</h5>
+            <img src="img/fb.png" alt="">
+            <img src="img/insta.png" alt="">
+            <img src="img/twitter.png" alt="">
+            <img class="tiktok" src="img/tiktok.png" alt="">
+            <img src="img/in.png" alt="">
+        </div>
+    </div>
+    <div class="row mt-2">
+        <p>Copyright @ 2023, KON Quiz All Right Reserved.</p>
+    </div>
+</footer>
+
+<style>
+    .footer{
+        background-color: #f8f9fa;
+    }
+
+    .footer img{
+        border-radius: 100%;
+        width: 50px;
+        height: 50px;
+        margin-right: 1vh;
+    }
+
+    .tiktok{
+        background-color: black;
+    }
+
+    .footer p{
+        font-size: 12px;
+    }
+</style>
