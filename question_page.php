6176e5d0bfbf20b0aca313acadb375d8d858340d
Ooi Zhen Zhi <tp060844@mail.apu.edu.my>
2023-05-04 18:22:08 +0800

    delete question

--- question_page.php
+++ question_page.php
@@ -102,10 +102,66 @@
     </div>
 </div>
 
+<!-- Confirm delete modal -->
+<div class="modal fade" id="delete-confirm" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog flex-column modal-dialog-centered">
+        <img class="modal-img-danger" src="img/Cave_Monkey.png" alt="">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="exampleModalLabel">Delete Quiz</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="toast align-items-center mx-auto border-0" id="alert" role="alert" aria-live="assertive" aria-atomic="true">
+                <div class="d-flex">
+                    <div class="toast-body">
+                    </div>
+                    <button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
+                </div>
+            </div>
+            <div class="modal-body">
+                <div class="mx-auto my-3">
+                    Are you sure you want to delete this?
+                </div>
+            </div>
+            <div class="modal-footer">
+                <form id="del">
+                    <input type="hidden" value="" name="ques_id">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">No</button>
+                    <button type="submit" class="btn btn-primary" name="">Yes</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
 <script>
     $('.form-check-input:checked').each(function() {
         $(this).parent('.form-check').addClass('checked');
     });
+
+    $('#del').submit(function(e) {
+            e.preventDefault();
+            var formData = $(this).serialize();
+            console.log(formData)
+            $.ajax({
+                type: 'POST',
+                url: 'del_ques.php',
+                data: formData,
+                success: function(response) {
+                    var data = JSON.parse(response);
+                    if (data.response == 'Success') {
+                            $('#alert').removeClass('text-bg-danger').addClass('text-bg-success');
+                            setTimeout(function() {
+                                window.location.href = 'question_page.php';
+                            }, 1000);
+                        } else {
+                            $('#alert').removeClass('text-bg-success').addClass('text-bg-danger');
+                        }
+                        $('#alert').find('.toast-body').html(data.message);
+                        bootstrap.Toast.getOrCreateInstance(document.getElementById('alert')).show();
+                }
+            });
+        });
 </script>
 
 <style>
@@ -124,6 +180,7 @@
 <script>
     $(".del-btn").click(function() {
         var btn_val = $(this).val();
-        alert(btn_val);
+        $("#delete-confirm input[name='ques_id']").val(btn_val);
+        $('#delete-confirm').modal('show');
     });
 </script>
