d59b3248f733d7e41ca6c1a46a5a3ac649a6fd17
Tunardo <justinkwy6246@gmail.com>
2023-05-04 17:22:44 +0800

    Update question_page.php

--- question_page.php
+++ question_page.php
@@ -1,4 +1,3 @@
-<!DOCTYPE html>
 <?php
     include("session.php");
     include("conn.php");
@@ -9,6 +8,7 @@
     else {
         $quiz_id = $_SESSION['quiz_id'];
     }
+    $count = 0;
     $id = $_SESSION['id'];
 
     $query = "SELECT * FROM quiz WHERE qz_ID = $quiz_id AND User_ID = $id";
@@ -20,179 +20,100 @@
     $question_result = mysqli_query($con, $query_ques);
 ?>
 
-<html>
-    <head>
-        <title>KON Quiz - Create Quiz</title>
-        <meta name="description" content="Our first page">
-        <meta name="keywords" content="html tutorial template">
-    </head>
-    
-    <body>
-    <br><br>
-    <div class="container-fluid pt-5 px-5 mx-auto">
-        <div class="shadow p-5">
-            <div class="d-flex justify-content-between align-items-center mb-3">
-                <h4 class="text-right"><?php echo $quiz_data['Title'] ?></h4> 
-            </div> 
-            <div class="container-fluid p-3">
-                <form action="create_ques.php">
-                    <button class="btn btn-primary btn-lg my-3" type="submit">+ADD NEW QUESTION</button>
-                </form>
-                
+<head>
+    <title>KON Quiz - Create Quiz</title>
+    <meta name="description" content="Our first page">
+    <meta name="keywords" content="html tutorial template">
+</head>
+
+<div class="container-fluid px-3">
+    <ul class="nav nav-tabs">
+        <li class="nav-item">
+            <a class="nav-link" href="homepage.php">Home</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="profile.php">Dashboard</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">View Question</a>
+        </li>
+    </ul>
+    <div class="shadow p-5 pt-4">
+        <h3><?php echo $quiz_data['Title'] ?></h3>
+
+        <!-- show all available question -->
+        <div class="row row-cols-1 row-cols-md-1 g-4">
+        <?php 
+            while($row=mysqli_fetch_array($question_result)) {
+                $count += 1;
+            ?>
+            <div class="col">
                 <div class="card">
                     <div class="card-header">
                         <div class="row">
-                            <h5 class="col">'. $row['ques'] . '</h5>
-                            <form action="edit_ques.php" method="POST">
-                                <button type="submit" class="btn btn-primary col-sm-1 mx-1">Edit</button>
-                                <button type="button" class="btn btn-primary col-sm-1">Delete</button>                    
-                                <input type="hidden" value="'. $row['ID'] .'">
-                            </form>
+                            <h5 class="col"><?php echo $count ?>. <?php echo $row['ques'] ?></h5>
                         </div>
                     </div>
-                    <div class="card-body mx-2">'. $row['opt1'] . '</div>
-                    <div class="card-body mx-2 correct-opt">'. $row['opt2'] . '</div>
-                    <div class="card-body mx-2">'. $row['opt3'] . '</div>
-                    <div class="card-body mx-2">'. $row['opt4'] . '</div>
-                </div>  
-                
-
-                <!-- show all available question -->
-                <?php 
-                    while($row=mysqli_fetch_array($question_result)) {
-                        if ($row['correct_opt'] == "opt1") {
-                            $question_block = '
-                                <div class="card">
-                                    <div class="card-header">
-                                        <div class="row">
-                                            <h5 class="col">'. $row['ques'] . '</h5>
-                                            <form action="edit_ques.php" method="POST">
-                                                <button type="submit" class="btn btn-primary col-sm-1 mx-1">Edit</button>
-                                                <button id="del_btn" type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#delete-confirm" value="'. $row['ID'] .'" onclick="">Delete</button>                       
-                                                <input type="hidden" value="'. $row['ID'] .'" name="ques_id">
-                                            </form>
-                                        </div>
-                                    </div>
-                                    <div class="card-body mx-2 correct-opt">'. $row['opt1'] . '</div>
-                                    <div class="card-body mx-2 ">'. $row['opt2'] . '</div>
-                                    <div class="card-body mx-2">'. $row['opt3'] . '</div>
-                                    <div class="card-body mx-2">'. $row['opt4'] . '</div>
-                                </div>  
-                            ';
-                        }
-                        else if ($row['correct_opt'] == "opt2") {
-                            $question_block = '
-                                <div class="card">
-                                    <div class="card-header">
-                                        <div class="row">
-                                            <h5 class="col">'. $row['ques'] . '</h5>
-                                            <form action="edit_ques.php" method="POST">
-                                                <button type="submit" class="btn btn-primary col-sm-1 mx-1">Edit</button>
-                                                <button id="del_btn" type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#delete-confirm" value="'. $row['ID'] .'" onclick="">Delete</button>                       
-                                                <input type="hidden" value="'. $row['ID'] .'" name="ques_id">
-                                            </form>
-                                        </div>
-                                    </div>
-                                    <div class="card-body mx-2">'. $row['opt1'] . '</div>
-                                    <div class="card-body mx-2 correct-opt">'. $row['opt2'] . '</div>
-                                    <div class="card-body mx-2">'. $row['opt3'] . '</div>
-                                    <div class="card-body mx-2">'. $row['opt4'] . '</div>
-                                </div>  
-                            ';
-                        }
-                        else if ($row['correct_opt'] == "opt3") {
-                            $question_block = '
-                                <div class="card">
-                                    <div class="card-header">
-                                        <div class="row">
-                                            <h5 class="col">'. $row['ques'] . '</h5>
-                                            <form action="edit_ques.php" method="POST">
-                                                <button type="submit" class="btn btn-primary col-sm-1 mx-1">Edit</button>
-                                                <button id="del_btn" type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#delete-confirm" value="'. $row['ID'] .'" onclick="">Delete</button>                    
-                                                <input type="hidden" value="'. $row['ID'] .'" name="ques_id">
-                                            </form>
-                                        </div>
-                                    </div>
-                                    <div class="card-body mx-2">'. $row['opt1'] . '</div>
-                                    <div class="card-body mx-2 ">'. $row['opt2'] . '</div>
-                                    <div class="card-body mx-2 correct-opt">'. $row['opt3'] . '</div>
-                                    <div class="card-body mx-2">'. $row['opt4'] . '</div>
-                                </div>  
-                            ';
-                        }
-                        else if ($row['correct_opt'] == "opt4") {
-                            $question_block = '
-                                <div class="card">
-                                    <div class="card-header">
-                                        <div class="row">
-                                            <h5 class="col">'. $row['ques'] . '</h5>
-                                            <form action="edit_ques.php" method="POST">
-                                                <button type="submit" class="btn btn-primary col-sm-1 mx-1">Edit</button>
-                                                <button id="del_btn" type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#delete-confirm" value="'. $row['ID'] .'" onclick="">Delete</button>                       
-                                                <input type="hidden" value="'. $row['ID'] .'" name="ques_id">
-                                            </form>
-                                        </div>
-                                    </div>
-                                    <div class="card-body mx-2">'. $row['opt1'] . '</div>
-                                    <div class="card-body mx-2 ">'. $row['opt2'] . '</div>
-                                    <div class="card-body mx-2">'. $row['opt3'] . '</div>
-                                    <div class="card-body mx-2 correct-opt">'. $row['opt4'] . '</div>
-                                </div>  
-                            ';
-                        }
-                        
-                        echo $question_block;
-                    }
-                ?>
-            </div>
-
-        </div>
-        </div>
-
-<!-- Confirm delete modal -->
-<div class="modal fade" id="delete-confirm" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog flex-column modal-dialog-centered">
-        <img class="modal-img-danger" src="img/Cave_Monkey.png" alt="">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h1 class="modal-title fs-5" id="exampleModalLabel">Delete Quiz</h1>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="toast align-items-center mx-auto border-0" id="alert" role="alert" aria-live="assertive" aria-atomic="true">
-                <div class="d-flex">
-                    <div class="toast-body">
+                    <div class="card-body" id="answer">
+                        <div class="form-check">
+                            <input class="form-check-input" type="checkbox" value="opt1" id="flexCheckDefault" <?php echo ($row['correct_opt'] == 'opt1') ? 'checked' : ''; ?> disabled>
+                            <label class="form-check-label" for="flexCheckDefault">
+                                <?php echo $row['opt1'] ?>
+                            </label>
+                        </div>
+                        <div class="form-check">
+                            <input class="form-check-input" type="checkbox" value="opt2" id="flexCheckDefault" <?php echo ($row['correct_opt'] == 'opt2') ? 'checked' : ''; ?> disabled>
+                            <label class="form-check-label" for="flexCheckDefault">
+                                <?php echo $row['opt2'] ?>
+                            </label>
+                        </div>
+                        <div class="form-check">
+                            <input class="form-check-input" type="checkbox" value="opt3" id="flexCheckDefault" <?php echo ($row['correct_opt'] == 'opt3') ? 'checked' : ''; ?> disabled>
+                            <label class="form-check-label" for="flexCheckDefault">
+                                <?php echo $row['opt3'] ?>
+                            </label>
+                        </div>
+                        <div class="form-check">
+                            <input class="form-check-input" type="checkbox" value="opt4" id="flexCheckDefault" <?php echo ($row['correct_opt'] == 'opt4') ? 'checked' : ''; ?> disabled>
+                            <label class="form-check-label" for="flexCheckDefault">
+                                <?php echo $row['opt4'] ?>
+                            </label>
+                        </div>
                     </div>
-                    <button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
+                    <div class="card-footer">
+                        <form action="edit_ques.php" method="POST">
+                            <button type="submit" class="btn btn-primary">Edit</button>
+                            <button id="del_btn" type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#delete-confirm" value="'. $row['ID'] .'" onclick="">Delete</button>                       
+                            <input type="hidden" value="'. $row['ID'] .'" name="ques_id">
+                        </form>
+                    </div>
                 </div>
             </div>
-            <div class="modal-body">
-                <input type="hidden" value="<?php echo $_SESSION['id'];?>" name="qz_id">
-                <div class="mx-auto my-3">
-                    Are you sure you want to delete this?
+            <?php
+            }
+            ?>
+
+            <div class="col">
+                <div class="card">
+                    <a type="button" class="btn btn-lg h-100" href="create_ques.php"><button class="btn fs-1 text-secondary border-0 h-100">+</button></a>
                 </div>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">No</button>
-                <button type="button" class="btn btn-primary" name="">Yes</button>
             </div>
         </div>
     </div>
 </div>
-    </body>
-</html>
+
+<script>
+    $('.form-check-input:checked').each(function() {
+        $(this).parent('.form-check').addClass('checked');
+    });
+</script>
 
 <style>
-    #ques {
-        height: 25vh;
-        resize: none;
+    .card{
+        height: 30vh;
     }
 
-    .tick {
-        height: 30px;
-        width: 30px;        
-    }
-
-    .correct-opt {
+    .checked{
         background-color: #9cff82;
     }
 
