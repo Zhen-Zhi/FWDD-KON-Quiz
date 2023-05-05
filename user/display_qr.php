66fbca8a95ae381732cf0a6cb3d241730e86a282
Ooi Zhen Zhi <tp060844@mail.apu.edu.my>
2023-05-05 14:28:31 +0800

    Create display_qr.php

---
+++ user/display_qr.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<?php 
+    include("../session.php");
+?>
+<html>
+    <head>
+        <title>KON Quiz</title>
+        <meta name="description" content="Our first page">
+        <meta name="keywords" content="html tutorial template">
+    </head>
+    
+    <body>
+        <div class="p-5 m-5">
+            <?php 
+                echo '<img src="https://quickchart.io/qr?text='.urlencode('http://192.168.100.15/FWDD-KON-QUIZ/homepage.php').'" width="150">';
+                //echo '<img src="http://quickchart.io/qr?text=http%3A%2F%2F192.168.100.15%2FFWDD_KON_QUIZ%2Fhomepage.php" width="150">';
+            ?>
+        </div>
+        </body>
+</html>
